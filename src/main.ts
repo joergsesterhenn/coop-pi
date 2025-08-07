7bfbec5bc13afdcaef40416792a58cc29f4c7556
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2025-08-08 00:14:25 +0200

    add coop components

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,20 @@ import App from './App.vue'
 
 import { firebaseApp } from './firebase'
 
-const app = createApp(App)
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+const vuetify = createVuetify({
+  theme: {
+    defaultTheme: 'dark',
+  },
+  components,
+  directives,
+})
+
+const app = createApp(App).use(vuetify)
 app.use(VueFire, {
   // imported above but could also just be created here
   firebaseApp,
