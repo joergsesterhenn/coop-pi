b2b26afd6c6bf9c69d33f0e9504ae960f5405a24
Jörg Sesterhenn <joerg.sesterhenn@gmail.com>
2025-08-07 23:40:36 +0200

    add firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,10 +1,17 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection } from 'firebase/firestore'
-// ... other firebase imports
 
-export const firebaseApp = initializeApp({
-  // your application settings
-})
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMHHM4pxTEanltJAigf2pkAx9YXb5eQoE',
+  authDomain: 'coop-pi.firebaseapp.com',
+  databaseURL: 'https://coop-pi-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'coop-pi',
+  storageBucket: 'coop-pi.firebasestorage.app',
+  messagingSenderId: '611841743382',
+  appId: '1:611841743382:web:6c542dbd6d947060867337',
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
 
 // used for the firestore refs
 const db = getFirestore(firebaseApp)
