e788677e9a1a33e5d3154d7cff4274dbf5f45a64
joergsesterhenn <joerg.sesterhenn@gmail.com>
2026-09-07 00:44:20 +0200

    fix: path to StreamCard

--- src/components/ChickenDashboard.vue
+++ src/components/ChickenDashboard.vue
@@ -12,7 +12,7 @@ import { onMounted } from "vue";
 import LightCard from "./LightCard.vue";
 import TempCard from "./TempCard.vue";
 import ImageCaptureCard from "./ImageCaptureCard.vue";
-import StremCard from "./StreamCaptureCard.vue";
+import StreamCard from "./StreamCard.vue";
 // import DoorCard from './DoorCard.vue'
 import SunCalc from "./SunCalc.vue";
 onMounted(() => {});
