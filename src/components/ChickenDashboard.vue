3292828ddff4c5f0972075de800fb2ee218ca471
joergsesterhenn <joerg.sesterhenn@gmail.com>
2026-09-10 01:54:13 +0200

    feat: remove image capture

--- src/components/ChickenDashboard.vue
+++ src/components/ChickenDashboard.vue
@@ -1,7 +1,6 @@
 <template>
   <DoorCard />
   <LightCard />
-  <ImageCaptureCard />
   <StreamCard />
   <TempCard />
   <SunCalc />
@@ -11,7 +10,6 @@
 import { onMounted } from "vue";
 import LightCard from "./LightCard.vue";
 import TempCard from "./TempCard.vue";
-import ImageCaptureCard from "./ImageCaptureCard.vue";
 import StreamCard from "./StreamCard.vue";
 // import DoorCard from './DoorCard.vue'
 import SunCalc from "./SunCalc.vue";
